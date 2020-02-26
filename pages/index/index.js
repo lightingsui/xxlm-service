3063aea63ddaf24fcf6628ccf25a9327927b1e30
lightingsui <coolsui@126.com>
2020-02-26 20:37:40 +0800

    resolve conflict

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,7 +5,6 @@ Page({
    * 页面的初始数据
    */
   data: {
-<<<<<<< HEAD
     phone: '',
     password: '',
     background: ["../../images/back-1.jpg", "../../images/back-2.jpg", "../../images/back-3.jpg"],
@@ -153,61 +152,3 @@ Page({
 
   }
 })
-=======
-    PageCur: 'notice'
-  },
-  NavChange(e) {
-    this.setData({
-      PageCur: e.currentTarget.dataset.cur
-    })
-  }
-  // data: {
-  //   motto: 'Hello World',
-  //   userInfo: {},
-  //   hasUserInfo: false,
-  //   canIUse: wx.canIUse('button.open-type.getUserInfo')
-  // },
-  // //事件处理函数
-  // bindViewTap: function() {
-  //   wx.navigateTo({
-  //     url: '../logs/logs'
-  //   })
-  // },
-  // onLoad: function () {
-  //   if (app.globalData.userInfo) {
-  //     this.setData({
-  //       userInfo: app.globalData.userInfo,
-  //       hasUserInfo: true
-  //     })
-  //   } else if (this.data.canIUse){
-  //     // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-  //     // 所以此处加入 callback 以防止这种情况
-  //     app.userInfoReadyCallback = res => {
-  //       this.setData({
-  //         userInfo: res.userInfo,
-  //         hasUserInfo: true
-  //       })
-  //     }
-  //   } else {
-  //     // 在没有 open-type=getUserInfo 版本的兼容处理
-  //     wx.getUserInfo({
-  //       success: res => {
-  //         app.globalData.userInfo = res.userInfo
-  //         this.setData({
-  //           userInfo: res.userInfo,
-  //           hasUserInfo: true
-  //         })
-  //       }
-  //     })
-  //   }
-  // },
-  // getUserInfo: function(e) {
-  //   console.log(e)
-  //   app.globalData.userInfo = e.detail.userInfo
-  //   this.setData({
-  //     userInfo: e.detail.userInfo,
-  //     hasUserInfo: true
-  //   })
-  // }
-})
->>>>>>> 5f36c8c0968e05d3d0559e1551ea7de179c74363
