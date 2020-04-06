7bd48098b1a40dbd02de0479a9414750a6d5591e
lightingsui <coolsui@126.com>
2020-04-06 11:59:13 +0800

    修改部分bug

--- pages/user/information/home/home.js
+++ pages/user/information/home/home.js
@@ -38,6 +38,19 @@ Component({
   },
 
   methods: {
+    clipboard: function (e) {
+      let text = e.currentTarget.dataset.text;
+      console.log(text);
+      wx.setClipboardData({
+        data: text,
+        success(res) {
+          wx.getClipboardData({
+            success(res) {
+            }
+          })
+        }
+      })
+    },
     // 查询所有详细信息
     getAllDetails:function() {
       let _this = this;
